8710bd8a548570dbd1050fde0aec4366866ad9ef
PrakharTagra <prakhartagra16@gmail.com>
2026-03-27 23:16:40 +0530

    Update VerifyEmail.jsx

--- frontend/src/pages/Auth/VerifyEmail.jsx
+++ frontend/src/pages/Auth/VerifyEmail.jsx
@@ -21,7 +21,7 @@ export function VerifyEmail() {
     (async () => {
       try {
         const res = await fetch(
-          `import.meta.env.VITE_API_URL/api/v1/auth/verify-email?token=${token}`
+          `${import.meta.env.VITE_API_URL}/api/v1/auth/verify-email?token=${token}`
         );
         const data = await res.json();
 
