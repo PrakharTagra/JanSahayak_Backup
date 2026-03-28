269690b5a713adeaf6a71bf32ae5d539d133b4d9
Ubuntu <ubuntu@ip-172-31-32-12.ap-south-1.compute.internal>
2026-03-28 01:59:05 +0000

    resolve index.js conflict

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,6 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// const database = require("./config/database");
-
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 4000;
-
-// // database connect
-// database.connect();
-
-// // middlewares
-// app.use(cors({
-//   origin: ["http://localhost:5173"],
-//   credentials: true,
-// }));
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // routes
-// app.use("https://candelaria-uninsinuative-obstructedly.ngrok-free.dev/api/v1/auth", require("./routes/auth"));
-// app.use("https://candelaria-uninsinuative-obstructedly.ngrok-free.dev/api/v1/complaint", require("./routes/complaint"));
-// app.use("https://candelaria-uninsinuative-obstructedly.ngrok-free.dev/api/v1/volunteer", require("./routes/volunteer"));
-// app.use("https://candelaria-uninsinuative-obstructedly.ngrok-free.dev/api/v1/government", require("./routes/government"));
-// app.use("https://candelaria-uninsinuative-obstructedly.ngrok-free.dev/api/v1/classify", require("./routes/classify"));
-
-// // default route
-// app.get("/", (req, res) => {
-//     return res.json({
-//         success: true,
-//         message: "Your app is running successfully",
-//     });
-// });
-// app.listen(PORT, () => {
-//     console.log(`Your server is up and running on PORT NUMBER ${PORT}`);
-// });
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const database = require("./config/database");
 
 dotenv.config();
@@ -53,6 +11,7 @@ const PORT = process.env.PORT || 4000;
 // database connect
 database.connect();
 
+<<<<<<< Updated upstream
 app.use(cors({
   origin: "https://jansahayak-rho.vercel.app",  // ✅ specific origin, not "*"
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -65,6 +24,8 @@ app.options("*", cors({
   credentials: true
 }));
 
+=======
+>>>>>>> Stashed changes
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
